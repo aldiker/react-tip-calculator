b9218eaf8b038119ea2b2a4d46edf9fa61dc16a3
aldiker <aldiker@ukr.net>
2024-01-08 16:43:28 +0200

    Added static html tags

--- src/App.js
+++ src/App.js
@@ -1,3 +1,37 @@
+import Bill from './components/Bill'
+
 export default function App() {
-    return <div>App</div>
+    return (
+        <div style={{ marginLeft: '10px' }}>
+            <div className='bill'>
+                <span>How much was the bill?</span>
+                <input type='number' style={{ marginLeft: '10px' }} />
+            </div>
+            <div className='valueService'>
+                <span>How did you like the service?</span>
+                <select value={0} style={{ marginLeft: '10px' }}>
+                    <option value={0}>Dissatisfied (0%)</option>
+                    <option value={5}>It was okay (5%)</option>
+                    <option value={10}>It was good (10%)</option>
+                    <option value={20}>Absolutely amazing! (20%)</option>
+                </select>
+            </div>
+            <div className='valueService'>
+                <span>How did your friend like the service?</span>
+                <select value={0} style={{ marginLeft: '10px' }}>
+                    <option value={0}>Dissatisfied (0%)</option>
+                    <option value={5}>It was okay (5%)</option>
+                    <option value={10}>It was good (10%)</option>
+                    <option value={20}>Absolutely amazing! (20%)</option>
+                </select>
+            </div>
+
+            <div
+                className='result'
+                style={{ margin: '10px 0px 10px 0px ', fontWeight: 'bold' }}>
+                You pay $105 ($100 + $5 tip)
+            </div>
+            <button>Reset</button>
+        </div>
+    )
 }
---
+++ src/components/Bill.js
@@ -0,0 +1,8 @@
+export default function Bill() {
+    return (
+        <div className='bill'>
+            <span>How much was the bill?</span>
+            <input type='number' style={{ marginLeft: '10px' }} />
+        </div>
+    )
+}
